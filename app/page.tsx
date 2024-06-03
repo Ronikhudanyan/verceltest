f7ae5f8ad71e9712982408466b0b6d25d077f355
“Ronikhudanyan” <Ronikhudanyan@gmail.com>
2024-06-03 13:37:13 -0700

    added questions

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            LOL{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -107,9 +107,38 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Boopity Boopity.
-          </p>
+        <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+        1.From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a brief explanation for each..
+          </h2>
+          <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+        2. What do you want to learn or do more of at work?
+          </h2>
+          <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+        3. Describe how you solved a challenge or technical issue that you faced in a previous role (preferably in a previous support role). How did you determine that your solution was successful?
+        </h2>
+        <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+        4.When would you choose to use Edge Functions, Serverless Functions, or Edge Middleware with Vercel?
+        </h2>
+        <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+        5. Imagine a customer writes in requesting help with a build issue on a framework or technology that you've not seen before. How would you begin troubleshooting this and what questions would you ask the customer to understand the situation better?
+        </h2>
+        <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+        6. The customer from question 5 replies to your response with the below:
+Im so frustrated. Ive been trying to make this work for hours and I just cant figure it out. It must be a platform issue so just fix it for me instead of asking me questions.” 
+Please write a follow-up reply to the customer.
+          </h2>
+          <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          7. A customer writes in to the Helpdesk asking "How do I do a redirect from the /blog path to https://example.com?" Please write a reply to the customer. Feel free to add any information about your decision making process after the reply.
+          </h2>
+          <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          8. A customer is creating a site and would like their project not to be indexed by search engines. Please write a reply to the customer. Feel free to add any information about your decision making process after the reply.
+          </h2>
+          <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          9. What do you think is one of the most common problems which customers ask Vercel for help with? How would you help customers to overcome common problems, short-term and long-term?
+          </h2>
+          <h2 className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          10. How could we improve or alter this familiarisation exercise?
+          </h2>
       </div>
     </main>
   );
